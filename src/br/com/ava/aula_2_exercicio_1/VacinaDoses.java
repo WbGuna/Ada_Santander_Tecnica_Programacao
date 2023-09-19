c556e922f806f28d65a5d41118dabcfa8bf7ea46
Willian_WB <wb.will.barbosa@gmail.com>
2023-09-18 21:07:01 -0300

    Colocando validação

--- src/br/com/ava/aula_2_exercicio_1/VacinaDoses.java
+++ src/br/com/ava/aula_2_exercicio_1/VacinaDoses.java
@@ -2,24 +2,29 @@ package br.com.ava.aula_2_exercicio_1;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class VacinaDoses {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Digite a data da primeira dose no formato dd/MM/yyyy:");
-		String firstDoseDateInput = scanner.nextLine();
+		String dataInicioDasDoses = sc.nextLine();
 
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		LocalDate dataPrimeiraDose = LocalDate.parse(firstDoseDateInput, formatter);
-
-		LocalDate dataSegundaDose = dataPrimeiraDose.plusMonths(1);
-		LocalDate dataTerceiraDose = dataSegundaDose.plusMonths(1);
-
-		System.out.println("Primeira dose : " + dataPrimeiraDose.format(formatter));
-		System.out.println("Segunda dose : " + dataSegundaDose.format(formatter));
-		System.out.println("Terceira dose : " + dataTerceiraDose.format(formatter));
+		DateTimeFormatter formatacao = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		try {			
+			LocalDate dataPrimeiraDose = LocalDate.parse(dataInicioDasDoses, formatacao);
+			
+			LocalDate dataSegundaDose = dataPrimeiraDose.plusMonths(1);
+			LocalDate dataTerceiraDose = dataSegundaDose.plusMonths(1);
+			
+			System.out.println("Primeira dose : " + dataPrimeiraDose.format(formatacao));
+			System.out.println("Segunda dose : " + dataSegundaDose.format(formatacao));
+			System.out.println("Terceira dose : " + dataTerceiraDose.format(formatacao));
+		} catch (DateTimeParseException e) {
+			 System.out.println("Formato inválido! Por favor, insira a data no formato dd/MM/yyyy.");
+		}
 		
-		scanner.close();
+		sc.close();
 	}
 }
